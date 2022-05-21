3bcd5b227d22b02ab2bbd4815a6b2c0c714986f1
Ashley Etchell <aetchell@gmail.com>
2022-05-21 13:54:18 +1200

    added colour options
    add highcharts colour options set

--- client/js/HighchartElemental.js
+++ client/js/HighchartElemental.js
@@ -1,5 +1,22 @@
 document.addEventListener(
     "DOMContentLoaded",
-    () => {}
+    () => {
+        if (typeof Highcharts === 'object') {
+            //console.log('Highcharts exists');
+            Highcharts.setOptions({
+                colors: [
+                    '#046b94', 
+                    '#53ab57', 
+                    '#1283b0', 
+                    '#9a7a5b', 
+                    '#67a7bf', 
+                    '#9fba4d',
+                    '#50a4a5', 
+                    '#d09861', 
+                    '#fcad30', 
+                    '#f13d0c'
+                ]
+            });
+        }
+    }
 );
-
