8d5d7d0d65b31d66b66db0bdf3035c871ab4c82b
Ashley@Pikselin <ash@pikselin.com>
2025-07-31 10:09:58 +1200

    Change to local Highcharts version from CDN

--- src/Libraries/HighchartsLibraries.php
+++ src/Libraries/HighchartsLibraries.php
@@ -7,15 +7,15 @@ namespace aetchell\Highcharts\Libraries {
     class HighchartsLibraries {
 
         public $HighchartsJSRequire = [];
-        private static $HighchartsURLBase = 'https://code.highcharts.com';
-
+        //private static $HighchartsURLBase = 'https://code.highcharts.com';
+        private static $HighchartsURLBase = '/_resources/vendor/aetchell/elemental-highchart/client/js/code/';
         public function Libraries($SiteConfig = false, $Extra = false) {
             $HighchartsJSRequire = [];
             // not currently used, here as a future placeholder
             //$HighchartsJSRequire[] = ['aetchell/elemental-highchart:client/js/HighchartElemental.js'];
 
             $HighchartAdditionalLibs = false;
-            
+
             $HighchartToUse = false;
 
             if (
@@ -23,12 +23,12 @@ namespace aetchell\Highcharts\Libraries {
             ) {
                 $HighchartToUse = $SiteConfig->HighchartVersionNumber;
             }
-           
+
             if (
                     $SiteConfig->HighchartAdditionalLibs != ''
             ) {
                 $HighchartAdditionalLibs = explode(',',$SiteConfig->HighchartAdditionalLibs);
-            }            
+            }
 
             /**
              * Create a map of files to include here, maybe add all these into the config page rather than hardcode into classes
@@ -43,42 +43,58 @@ namespace aetchell\Highcharts\Libraries {
 
                     case 'stock':
                     case 'chart':
-                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                [self::$HighchartsURLBase, 'stock', $HighchartToUse, 'highstock.js'] :
-                                [self::$HighchartsURLBase, 'stock', 'highstock.js']);
+//                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                [self::$HighchartsURLBase, 'stock', $HighchartToUse, 'highstock.js'] :
+//                                [self::$HighchartsURLBase, 'stock', 'highstock.js']);
+//
+//                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                [self::$HighchartsURLBase, 'stock', $HighchartToUse, 'highcharts-more.js'] :
+//                                [self::$HighchartsURLBase, 'stock', 'highcharts-more.js']);
+//
+//                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/exporting.js'] :
+//                                [self::$HighchartsURLBase, 'modules/exporting.js']);
+//
+//                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/data.js'] :
+//                                [self::$HighchartsURLBase, 'modules/data.js']);
+//
+//                        if (isset($Extra['Exporting']) && $Extra['Exporting'] == true) {
+//                            $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/exporting.js'] :
+//                                [self::$HighchartsURLBase, 'modules/exporting.js']);
+//
+//                            $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/offline-exporting.js'] :
+//                                [self::$HighchartsURLBase, 'modules/offline-exporting.js']);
+//
+//                            $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/export-data.js'] :
+//                                [self::$HighchartsURLBase, 'modules/export-data.js']);
+//                        }
+//                        if(is_array($HighchartAdditionalLibs) && count($HighchartAdditionalLibs) >= 1) {
+//                            foreach($HighchartAdditionalLibs as $extraFile) {
+//                                $HighchartsJSRequire[] = ($HighchartToUse !== false ?
+//                                        [self::$HighchartsURLBase, $HighchartToUse, $extraFile] :
+//                                        [self::$HighchartsURLBase, $extraFile]);
+//                            }
+//                        }
 
-                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                [self::$HighchartsURLBase, 'stock', $HighchartToUse, 'highcharts-more.js'] :
-                                [self::$HighchartsURLBase, 'stock', 'highcharts-more.js']);
+                    $HighchartsJSRequire[] = [self::$HighchartsURLBase,  'highstock.js'];
+                    $HighchartsJSRequire[] = [self::$HighchartsURLBase, 'highcharts-more.js'];
+                    $HighchartsJSRequire[] =  [self::$HighchartsURLBase, 'modules/exporting.js'];
+                    $HighchartsJSRequire[] = [self::$HighchartsURLBase, 'modules/data.js'];
 
-                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/exporting.js'] :
-                                [self::$HighchartsURLBase, 'modules/exporting.js']);
-
-                        $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/data.js'] :
-                                [self::$HighchartsURLBase, 'modules/data.js']);
-                        
-                        if (isset($Extra['Exporting']) && $Extra['Exporting'] == true) {
-                            $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/exporting.js'] :
-                                [self::$HighchartsURLBase, 'modules/exporting.js']);                          
-                            
-                            $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/offline-exporting.js'] :
-                                [self::$HighchartsURLBase, 'modules/offline-exporting.js']);  
-
-                            $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                [self::$HighchartsURLBase, $HighchartToUse, 'modules/export-data.js'] :
-                                [self::$HighchartsURLBase, 'modules/export-data.js']);                              
+                    if (isset($Extra['Exporting']) && $Extra['Exporting'] == true) {
+                        $HighchartsJSRequire[] = [self::$HighchartsURLBase, 'modules/exporting.js'];
+                        $HighchartsJSRequire[] = [self::$HighchartsURLBase, 'modules/offline-exporting.js'];
+                        $HighchartsJSRequire[] = [self::$HighchartsURLBase, 'modules/export-data.js'];
+                    }
+                    if(is_array($HighchartAdditionalLibs) && count($HighchartAdditionalLibs) >= 1) {
+                        foreach($HighchartAdditionalLibs as $extraFile) {
+                            $HighchartsJSRequire[] = [self::$HighchartsURLBase, $extraFile];
                         }
-                        if(is_array($HighchartAdditionalLibs) && count($HighchartAdditionalLibs) >= 1) {
-                            foreach($HighchartAdditionalLibs as $extraFile) {
-                                $HighchartsJSRequire[] = ($HighchartToUse !== false ?
-                                        [self::$HighchartsURLBase, $HighchartToUse, $extraFile] :
-                                        [self::$HighchartsURLBase, $extraFile]);                                
-                            }
-                        }                        
+                    }
 
 
 
